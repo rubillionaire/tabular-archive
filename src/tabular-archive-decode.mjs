ab65b142a83be7af94363cd7c3ad5d431bc96abc
rubonics <rubenrodriguez+github@gmail.com>
2023-09-27 22:16:28 -0400

    yeild the current row index for consumer that are interested in understanding their position in total set of rows

--- src/tabular-archive-decode.mjs
+++ src/tabular-archive-decode.mjs
@@ -212,7 +212,7 @@ export const decode = ({ readRange }) => async ({ archiveFilePath }) => {
       const compressedBufferSlice = compressedBuffer.slice(offsetStart, offsetEnd)
       const buffer = gunzipSync(compressedBufferSlice)
       const { row } = rowDecoder({ buffer })
-      yield { row }
+      yield { row, index: startRowNumber + i }
       offsetStart = offsetEnd
     }
   }
